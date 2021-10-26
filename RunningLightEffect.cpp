e5b82e2b942b91261b4644e6e44e241892a4b07a
Valentijn <valentijn@mindsoup.net>
2021-10-27 00:01:54 +0200

    slightly slow down running lights

--- RunningLightEffect.cpp
+++ RunningLightEffect.cpp
@@ -19,7 +19,7 @@ void RunningLightEffect::loop(VolumeContext &context) {
 	context.ledStrip.strip->fill(context.ledStrip.strip->Color(0,0,0), 0, context.ledStrip.numLeds);
 
 	if(context.volume > 0.2 && (this->root == 0 || this->root->getChainLength() < 5) && timestampOfLastEffect + 300 < millis()) {
-		RunningLight* light = new RunningLight(0, 120, 5, 1000, new RangeColorGenerator(), this);
+		RunningLight* light = new RunningLight(0, 80, 5, 1300, new RangeColorGenerator(), this);
 		if(this->root == 0) {
 			this->root = light;
 		} else {
